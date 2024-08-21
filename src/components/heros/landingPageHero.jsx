cef82f065c7dbee3b33fa101358d140d4a621555
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-08-21 23:31:52 +0100

    Create landingPageHero.jsx

---
+++ src/components/heros/landingPageHero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import BackgroundVideo from 'next-video/background-video';
+import meet from '../../../videos/meet.mp4';
+
+export default function LandingPageHero() {
+        return (
+                <BackgroundVideo src={meet}>
+                        <h1>next-video</h1>
+                        <p>
+                                A React component for adding video to your Next.js application.
+                                It extends both the video element and your Next app with features
+                                for automatic video optimization.
+                        </p>
+                </BackgroundVideo>
+        )
+}
