2b6f2fafb0eb87697a1b5fd54520e6236b8c7a79
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-08-21 15:58:36 +0100

    Create scrollingTag.jsx

---
+++ src/components/footer/scrollingTag.jsx
@@ -0,0 +1,19 @@
+
+export default function ScrollingTag() {
+        return (
+                <div className='flex'>
+                        <span>
+                                Empowering automation, Driving AI Efficiency
+                        </span>
+                        <span>
+                                .
+                        </span>
+                        <span>
+                                Empowering automation, Driving AI Efficiency
+                        </span>
+                        <span>
+                                .
+                        </span>
+                </div>
+        )
+}
