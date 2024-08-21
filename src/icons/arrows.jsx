925f240ffcb2700e15241effc12419f48b88c054
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-08-21 15:58:26 +0100

    Create arrows.jsx

---
+++ src/icons/arrows.jsx
@@ -0,0 +1,20 @@
+export const RightArrow = ({ className }) => {
+        return (
+                <svg 
+                        xmlns="https://www.w3.org/2000/svg" 
+                        width="1em" 
+                        height="1em" 
+                        viewBox="0 0 24 24"
+                        className={className}
+                >
+                        <path 
+                                fill="none" 
+                                stroke="currentColor" 
+                                strokeLinecap="round" 
+                                strokeLinejoin="round" 
+                                strokeWidth={1} 
+                                d="M19 12H5m14 0l-4 4m4-4l-4-4"
+                        />
+                </svg>
+        )
+}
