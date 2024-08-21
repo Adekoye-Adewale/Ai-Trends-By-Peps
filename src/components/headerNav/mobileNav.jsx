4b7da66aaa8647f2dece2251b4531a9e3a65fce0
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-08-21 15:59:34 +0100

    Create mobileNav.jsx

---
+++ src/components/headerNav/mobileNav.jsx
@@ -0,0 +1,48 @@
+'use client'
+import React, { useState } from 'react'
+import { menuLinks } from '../headerNavCopy'
+import Link from 'next/link'
+import PriBtn from '../button/priBtn'
+
+export default function MobileNav({ onClick, openNav }) {
+
+        return (
+                <div className={`min-[900px]:hidden fixed top-0 left-full right-0 flex flex-col gap-5 p-10 max-w-[800px] w-[95%] h-screen bg-LightColor-100 rounded-l-2xl border border-solid border-LightColor-300 transition-all duration-300 ${openNav ? 'translate-x-[-99%]' : 'translate-x-[10%]'}`} >
+                        <span 
+                                className='self-end text-xs font-semibold uppercase cursor-pointer'
+                                onClick={onClick}
+                        >
+                                close
+                        </span>
+                        <Nav/>
+                        <div className='mt-auto'>
+                                 <PriBtn
+                                        label={'Register Now'}
+                                        href={'/'}
+                                />
+                        </div>
+                </div>
+        )
+}
+
+const Nav = () => {
+        return (
+                <nav>
+                        <ul className='grid text-base divide-y divide-solid divide-LightColor-500'>
+                                {menuLinks.map((items) => (
+                                        <li 
+                                                key={items.id}
+                                                className='py-5'
+                                        >
+                                                <Link
+                                                        href={items.url}
+                                                        title={items.name}
+                                                >
+                                                        {items.name}
+                                                </Link>
+                                        </li>
+                                ))}
+                        </ul>
+                </nav>
+        )
+}
