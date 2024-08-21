c5ab17226619601bcfadd0074b44703efb870dba
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-08-21 15:58:30 +0100

    Create headerNavCopy.js

---
+++ src/components/headerNavCopy.js
@@ -0,0 +1,32 @@
+export const menuLinks = [
+        { 
+                id: 1, 
+                name: 'Home', 
+                url: '/',
+        },
+        { 
+                id: 2, 
+                name: 'About', 
+                url: '/#about',
+        },
+        { 
+                id: 3, 
+                name: 'Benefits', 
+                url: '/#benefits',
+        },
+        { 
+                id: 4, 
+                name: 'Testimonials', 
+                url: '/#testimonials',
+        },
+        { 
+                id: 5, 
+                name: 'Products', 
+                url: '/#products',
+        },
+        { 
+                id: 6, 
+                name: 'Courses', 
+                url: '/#courses',
+        },
+]
