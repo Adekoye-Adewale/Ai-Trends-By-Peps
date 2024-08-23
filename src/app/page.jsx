6df335d87f7ccc3f2cfd47e52b55917f164d5d84
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-08-23 10:48:59 +0100

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,7 @@ import LandingPageHero from "@/components/heros/landingPageHero";
 import AboutSection from "@/components/landingPage/aboutSection";
 import BenefitsSection from "@/components/landingPage/benefitsSection";
 import Programs from "@/components/landingPage/programs";
+import TestimonialSection from "@/components/landingPage/testimonialSection";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <AboutSection/>
       <Programs/>
       <BenefitsSection/>
+      <TestimonialSection/>
     </main>
   );
 }
