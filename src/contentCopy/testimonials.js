e50367a4bde0232291e624fbaa6311cea24a1ada
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-08-23 10:49:10 +0100

    Create testimonials.js

---
+++ src/contentCopy/testimonials.js
@@ -0,0 +1,41 @@
+export const TestimonialCopy = [
+        {
+                id: 1,
+                name: "Kelvin Watt",
+                role: `Freelance Web Developer`,
+                testimony: `I learnt how to use AI to my benefit, writing productive and optimized code, also building a better profile on LinkedIn, and it gave me the confidence to write content.`,
+                img: {
+                        src: `/images/john-doe01.webp`,
+                        alt: `Kelvin Watt`,
+                        title: `Kelvin Watt`,
+                        width: 50,
+                        height: 50,
+                }
+        },
+        {
+                id: 2,
+                name: "Greer Yeaton",
+                role: `Human Resource Manager`,
+                testimony: `I learnt how to use AI to my benefit, writing productive and optimized code, also building a better profile on LinkedIn, and it gave me the confidence to write content.`,
+                img: {
+                        src: `/images/female-john-doe.webp`,
+                        alt: `Greer Yeaton`,
+                        title: `Greer Yeaton`,
+                        width: 50,
+                        height: 50,
+                }
+        },
+        {
+                id: 3,
+                name: "Rauno Freiberg",
+                role: `Digital Marketer`,
+                testimony: `I learnt how to use AI to my benefit, writing productive and optimized code, also building a better profile on LinkedIn, and it gave me the confidence to write content.`,
+                img: {
+                        src: `/images/rauno-freiberg.webp`,
+                        alt: `Rauno Freiberg`,
+                        title: `Rauno Freiberg`,
+                        width: 50,
+                        height: 50,
+                }
+        },
+]
