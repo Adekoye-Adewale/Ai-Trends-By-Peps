e0dfe3858b1cd4e109209d1f8ccc595639576778
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-08-26 23:26:00 +0100

    Update headline.jsx

--- src/components/text/headline.jsx
+++ src/components/text/headline.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Headline({ label }) {
         return (
-                <h1 className='text-5xl sm:text-8xl font-black capitalize text-DarkColor-700 '>
+                <h1 className='text-4xl md:text-5xl lg:text-8xl font-black capitalize text-DarkColor-700 '>
                         {label}
                 </h1>
         )
