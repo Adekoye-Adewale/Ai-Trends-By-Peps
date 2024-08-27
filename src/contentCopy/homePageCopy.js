f1ee2b9641627e8b3fe5d76d3e0e7f9732c080a1
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-08-27 10:34:07 +0100

    Update homePageCopy.js

--- src/contentCopy/homePageCopy.js
+++ src/contentCopy/homePageCopy.js
@@ -1,5 +1,5 @@
 export const homeHero = {
-        headline: `Empowering Automation, Driving AI Efficiency`,
+        headline: `Join the top 1% league`,
         desc: `Unlock the Potential of Generative AI for Your Business`,
         ctaLabel: `Get Started Today`,
         ctaLink: `/sign-up`,
@@ -102,8 +102,8 @@ export const homeAbtSec = {
 
 export const homeBenefitSec = {
         title: `Why Ai Trends?`,
-        desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s`,
-        subTitle: `Talent is key. Support is power. Mentorship create superhero`,
+        desc: `AI Trends is a platform for mastering generative AI and automation. It caters to tech enthusiasts, business professionals, and enterprise leaders, offering tools, insights, and a community for leveraging AI to achieve efficiency and innovation.`,
+        subTitle: `Talent is key. Support is power. Mentorship creates superheroes.`,
         subTitleDesc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum`,
         img: {
                 src: `/images/about-Ai_trends.webp`,
@@ -118,24 +118,60 @@ export const homeBenefitSec = {
                         icon: `<svg xmlns="https://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 48 48">
                                         <path fill="currentColor" d="M15 17a6 6 0 1 0 0-12a6 6 0 0 0 0 12m18 0a6 6 0 1 0 0-12a6 6 0 0 0 0 12M4 22.446A3.446 3.446 0 0 1 7.446 19h9.624A7.96 7.96 0 0 0 16 23a7.98 7.98 0 0 0 2.708 6h-2.262a5.44 5.44 0 0 0-4.707 2.705c-3.222-.632-5.18-2.203-6.32-3.968C4 25.54 4 23.27 4 22.877zM31.554 29a5.44 5.44 0 0 1 4.707 2.705c3.222-.632 5.18-2.203 6.32-3.968C44 25.54 44 23.27 44 22.877v-.43A3.446 3.446 0 0 0 40.554 19H30.93A7.96 7.96 0 0 1 32 23a7.98 7.98 0 0 1-2.708 6zM30 23a6 6 0 1 1-12 0a6 6 0 0 1 12 0M13 34.446A3.446 3.446 0 0 1 16.446 31h15.108A3.446 3.446 0 0 1 35 34.446v.431c0 .394 0 2.663-1.419 4.86C32.098 42.033 29.233 44 24 44s-8.098-1.967-9.581-4.263C13 37.54 13 35.27 13 34.877z"/>
                                 </svg>`,
-                        title: `Career Opportunities`,
-                        desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum`,
+                        img: {
+                                src: `/images/icons/writing.png`,
+                                alt: `Educational Content`,
+                                title: `Educational Content`,
+                                width: 75,
+                                height: 75,
+                        },
+                        title: `Educational Content`,
+                        desc: `Stay ahead of the curve with our comprehensive articles, tutorials, and case studies. Our content covers everything from AI fundamentals to advanced applications, ensuring you have the knowledge to implement AI effectively`,
                 },
                 {
                         id: 2,
                         icon: `<svg xmlns="https://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 48 48">
                                         <path fill="currentColor" d="M15 17a6 6 0 1 0 0-12a6 6 0 0 0 0 12m18 0a6 6 0 1 0 0-12a6 6 0 0 0 0 12M4 22.446A3.446 3.446 0 0 1 7.446 19h9.624A7.96 7.96 0 0 0 16 23a7.98 7.98 0 0 0 2.708 6h-2.262a5.44 5.44 0 0 0-4.707 2.705c-3.222-.632-5.18-2.203-6.32-3.968C4 25.54 4 23.27 4 22.877zM31.554 29a5.44 5.44 0 0 1 4.707 2.705c3.222-.632 5.18-2.203 6.32-3.968C44 25.54 44 23.27 44 22.877v-.43A3.446 3.446 0 0 0 40.554 19H30.93A7.96 7.96 0 0 1 32 23a7.98 7.98 0 0 1-2.708 6zM30 23a6 6 0 1 1-12 0a6 6 0 0 1 12 0M13 34.446A3.446 3.446 0 0 1 16.446 31h15.108A3.446 3.446 0 0 1 35 34.446v.431c0 .394 0 2.663-1.419 4.86C32.098 42.033 29.233 44 24 44s-8.098-1.967-9.581-4.263C13 37.54 13 35.27 13 34.877z"/>
                                 </svg>`,
-                        title: `Practical Approach`,
-                        desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum`,
+                        img: {
+                                src: `/images/icons/mail.png`,
+                                alt: `Community and Newsletter`,
+                                title: `Community and Newsletter`,
+                                width: 75,
+                                height: 75,
+                        },
+                        title: `Community and Newsletter`,
+                        desc: `Join our vibrant community of AI enthusiasts and professionals. Subscribe to our newsletter for exclusive updates, tips, and insights on the latest AI tools and trends`,
                 },
                 {
                         id: 3,
                         icon: `<svg xmlns="https://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 48 48">
                                         <path fill="currentColor" d="M15 17a6 6 0 1 0 0-12a6 6 0 0 0 0 12m18 0a6 6 0 1 0 0-12a6 6 0 0 0 0 12M4 22.446A3.446 3.446 0 0 1 7.446 19h9.624A7.96 7.96 0 0 0 16 23a7.98 7.98 0 0 0 2.708 6h-2.262a5.44 5.44 0 0 0-4.707 2.705c-3.222-.632-5.18-2.203-6.32-3.968C4 25.54 4 23.27 4 22.877zM31.554 29a5.44 5.44 0 0 1 4.707 2.705c3.222-.632 5.18-2.203 6.32-3.968C44 25.54 44 23.27 44 22.877v-.43A3.446 3.446 0 0 0 40.554 19H30.93A7.96 7.96 0 0 1 32 23a7.98 7.98 0 0 1-2.708 6zM30 23a6 6 0 1 1-12 0a6 6 0 0 1 12 0M13 34.446A3.446 3.446 0 0 1 16.446 31h15.108A3.446 3.446 0 0 1 35 34.446v.431c0 .394 0 2.663-1.419 4.86C32.098 42.033 29.233 44 24 44s-8.098-1.967-9.581-4.263C13 37.54 13 35.27 13 34.877z"/>
                                 </svg>`,
-                        title: `Community at the Center`,
-                        desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum`,
+                        img: {
+                                src: `/images/icons/mail.png`,
+                                alt: `Educational Content`,
+                                title: `Educational Content`,
+                                width: 75,
+                                height: 75,
+                        },
+                        title: `Consultancy Services`,
+                        desc: `Get personalized consulting from our AI experts. We offer tailored solutions to help your business integrate AI for process automation, from initial strategy to full implementation.`,
+                },
+                {
+                        id: 4,
+                        icon: `<svg xmlns="https://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 48 48">
+                                        <path fill="currentColor" d="M15 17a6 6 0 1 0 0-12a6 6 0 0 0 0 12m18 0a6 6 0 1 0 0-12a6 6 0 0 0 0 12M4 22.446A3.446 3.446 0 0 1 7.446 19h9.624A7.96 7.96 0 0 0 16 23a7.98 7.98 0 0 0 2.708 6h-2.262a5.44 5.44 0 0 0-4.707 2.705c-3.222-.632-5.18-2.203-6.32-3.968C4 25.54 4 23.27 4 22.877zM31.554 29a5.44 5.44 0 0 1 4.707 2.705c3.222-.632 5.18-2.203 6.32-3.968C44 25.54 44 23.27 44 22.877v-.43A3.446 3.446 0 0 0 40.554 19H30.93A7.96 7.96 0 0 1 32 23a7.98 7.98 0 0 1-2.708 6zM30 23a6 6 0 1 1-12 0a6 6 0 0 1 12 0M13 34.446A3.446 3.446 0 0 1 16.446 31h15.108A3.446 3.446 0 0 1 35 34.446v.431c0 .394 0 2.663-1.419 4.86C32.098 42.033 29.233 44 24 44s-8.098-1.967-9.581-4.263C13 37.54 13 35.27 13 34.877z"/>
+                                </svg>`,
+                        img: {
+                                src: `/images/icons/mail.png`,
+                                alt: `Educational Content`,
+                                title: `Educational Content`,
+                                width: 75,
+                                height: 75,
+                        },
+                        title: `Digital Products`,
+                        desc: ` Explore our range of digital products designed to make AI accessible and practical. From e-books and templates to AI toolkits and online courses, we provide the resources you need to succeed.`,
                 },
         ]
 }
