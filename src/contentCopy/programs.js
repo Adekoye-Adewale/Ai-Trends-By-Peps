fcd8b159c0b9663ab04ceb4421c3c9aa33c845a2
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-08-27 10:34:12 +0100

    Update programs.js

--- src/contentCopy/programs.js
+++ src/contentCopy/programs.js
@@ -1,5 +1,5 @@
 export const Courses = {
-        title: 'Explore our programs',
+        title: 'Explore our resources',
         program: [
                 {
                         icon: `<svg xmlns="https://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 48 48">
@@ -12,17 +12,17 @@ export const Courses = {
                                 </svg>`,
                         img: {
                                 src: 'https://picsum.photos/200/300',
-                                alt: 'Educational Content',
-                                title: 'Educational Content',
+                                alt: 'E-Books and Guides',
+                                title: 'E-Books and Guides',
                                 width: 200,
                                 height: 300,
                         },
-                        title: `Educational Content`,
+                        title: `E-Books and Guides`,
                         cta: {
                                 label: `See Programs`,
                                 href: `/Educational Content`,
                         },
-                        desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s`,
+                        desc: `Download our expertly crafted e-books and guides to deepen your understanding of AI and its applications. Topics include AI basics, advanced techniques, and sector-specific use cases.`,
                         duration: `24 weeks`,
                         startingDate: `Dec 2024`,
                         platform: `Online`,
@@ -38,17 +38,17 @@ export const Courses = {
                                 </svg>`,
                         img: {
                                 src: 'https://picsum.photos/200/300',
-                                alt: 'Community and Newsletter',
-                                title: 'Community and Newsletter',
+                                alt: 'Templates and Toolkits',
+                                title: 'Templates and Toolkits',
                                 width: 200,
                                 height: 300,
                         },
-                        title: `Community and Newsletter`,
+                        title: `Templates and Toolkits`,
                         cta: {
                                 label: `See Programs`,
                                 href: `/community-and-newsletter`,
                         },
-                        desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s`,
+                        desc: `Access our customizable templates and toolkits designed to streamline your AI projects. These resources are practical, easy to use, and tailored to various industries.`,
                         duration: `24 weeks`,
                         startingDate: `Dec 2024`,
                         platform: `Online`,
@@ -64,69 +64,17 @@ export const Courses = {
                                 </svg>`,
                         img: {
                                 src: 'https://picsum.photos/200/300',
-                                alt: 'Consultancy Services',
-                                title: 'Consultancy Services',
+                                alt: 'Online Courses',
+                                title: 'Online Courses',
                                 width: 200,
                                 height: 300,
                         },
-                        title: `Consultancy Services`,
+                        title: `Online Courses`,
                         cta: {
                                 label: `See Programs`,
                                 href: `/Consultancy Services`,
                         },
-                        desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s`,
-                        duration: `24 weeks`,
-                        startingDate: `Dec 2024`,
-                        platform: `Online`,
-                },
-                {
-                        icon: `<svg xmlns="https://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 48 48">
-                                        <g fill="currentColor">
-                                                <path fillRule="evenodd" d="M9.263 6c-.378 0-.715.262-.845.656L6.11 13.667a2.2 2.2 0 0 0-.11.687v2.789C6 18.72 7.151 20 8.571 20s2.572-1.28 2.572-2.857c0 1.578 1.151 2.857 2.571 2.857s2.572-1.28 2.572-2.857c0 1.578 1.151 2.857 2.571 2.857s2.57-1.278 2.572-2.855C21.429 18.722 22.58 20 24 20s2.571-1.28 2.571-2.857c0 1.578 1.152 2.857 2.572 2.857s2.57-1.278 2.571-2.855C31.715 18.722 32.866 20 34.286 20s2.571-1.28 2.571-2.857c0 1.578 1.151 2.857 2.572 2.857C40.849 20 42 18.72 42 17.143v-2.789a2.2 2.2 0 0 0-.11-.687l-2.308-7.01c-.13-.395-.467-.657-.845-.657z" clipRule="evenodd"/>
-                                                <path fillRule="evenodd" d="M10 21.23V29H7.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h33a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5H38v-7.77a3.9 3.9 0 0 1-1.143-.703a4 4 0 0 1-.857.576V29H12v-7.897a4 4 0 0 1-.857-.576c-.336.295-.72.535-1.143.703m26-1.957q.127-.127.24-.273H36zM37.474 19a2.8 2.8 0 0 0 .526.519V19zM10 19.519a2.6 2.6 0 0 0 .526-.519H10zM11.76 19H12v.273a3 3 0 0 1-.24-.273M8.5 33a.5.5 0 0 0-.5.5V41a1 1 0 0 0 1 1h30a1 1 0 0 0 1-1v-7.5a.5.5 0 0 0-.5-.5z" clipRule="evenodd"/>
-                                                <path d="M14 26.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5z"/>
-                                                <path d="M16 27.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5zm8 0a1.5 1.5 0 1 1-3 0a1.5 1.5 0 0 1 3 0"/>
-                                        </g>
-                                </svg>`,
-                        img: {
-                                src: 'https://picsum.photos/200/300',
-                                alt: 'Digital Products',
-                                title: 'Digital Products',
-                                width: 200,
-                                height: 300,
-                        },
-                        title: `Digital Products`,
-                        cta: {
-                                label: `See Programs`,
-                                href: `/Digital Products`,
-                        },
-                        desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s`,
-                        duration: `24 weeks`,
-                        startingDate: `Dec 2024`,
-                        platform: `Online`,
-                },
-                {
-                        icon: `<svg xmlns="https://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 48 48">
-                                        <g fill="currentColor">
-                                                <path fillRule="evenodd" d="M9.263 6c-.378 0-.715.262-.845.656L6.11 13.667a2.2 2.2 0 0 0-.11.687v2.789C6 18.72 7.151 20 8.571 20s2.572-1.28 2.572-2.857c0 1.578 1.151 2.857 2.571 2.857s2.572-1.28 2.572-2.857c0 1.578 1.151 2.857 2.571 2.857s2.57-1.278 2.572-2.855C21.429 18.722 22.58 20 24 20s2.571-1.28 2.571-2.857c0 1.578 1.152 2.857 2.572 2.857s2.57-1.278 2.571-2.855C31.715 18.722 32.866 20 34.286 20s2.571-1.28 2.571-2.857c0 1.578 1.151 2.857 2.572 2.857C40.849 20 42 18.72 42 17.143v-2.789a2.2 2.2 0 0 0-.11-.687l-2.308-7.01c-.13-.395-.467-.657-.845-.657z" clipRule="evenodd"/>
-                                                <path fillRule="evenodd" d="M10 21.23V29H7.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h33a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5H38v-7.77a3.9 3.9 0 0 1-1.143-.703a4 4 0 0 1-.857.576V29H12v-7.897a4 4 0 0 1-.857-.576c-.336.295-.72.535-1.143.703m26-1.957q.127-.127.24-.273H36zM37.474 19a2.8 2.8 0 0 0 .526.519V19zM10 19.519a2.6 2.6 0 0 0 .526-.519H10zM11.76 19H12v.273a3 3 0 0 1-.24-.273M8.5 33a.5.5 0 0 0-.5.5V41a1 1 0 0 0 1 1h30a1 1 0 0 0 1-1v-7.5a.5.5 0 0 0-.5-.5z" clipRule="evenodd"/>
-                                                <path d="M14 26.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5z"/>
-                                                <path d="M16 27.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5zm8 0a1.5 1.5 0 1 1-3 0a1.5 1.5 0 0 1 3 0"/>
-                                        </g>
-                                </svg>`,
-                        img: {
-                                src: 'https://picsum.photos/200/300',
-                                alt: 'Social Media',
-                                title: 'Social Media',
-                                width: 200,
-                                height: 300,
-                        },
-                        title: `Social Media`,
-                        cta: {
-                                label: `See Programs`,
-                                href: `/social-media`,
-                        },
-                        desc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s`,
+                        desc: `Enroll in our online courses to gain in-depth knowledge and hands-on experience with AI tools and technologies. Our courses are designed for all skill levels, from beginners to advanced practitioners`,
                         duration: `24 weeks`,
                         startingDate: `Dec 2024`,
                         platform: `Online`,
