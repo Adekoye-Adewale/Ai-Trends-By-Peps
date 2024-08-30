e6b8806c697d9c4cd1bab94c85e87f42171695fc
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-08-30 22:11:01 +0100

    Update newsletter.jsx

--- src/components/forms/newsletter.jsx
+++ src/components/forms/newsletter.jsx
@@ -46,45 +46,48 @@ export default function Newsletter() {
                         >
                                 Your email address
                         </label>
-                        <input
-                                type="email"
-                                name="email"
-                                id="email"
-                                placeholder='sample@email.com'
-                                {...register("email", {
-                                        required: "Email Address is required",
-                                        pattern: {
-                                                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/,
-                                                message: "Please enter a valid email address"
-                                        },
-                                        minLength: {
-                                                value: 7,
-                                                message: "Email must be at least 7 characters long"
-                                        },
-                                        maxLength: {
-                                                value: 50,
-                                                message: "Email cannot exceed 50 characters"
-                                        },
-                                        // validate: {
-                                        //         notGmail: value =>
-                                        //                 !value.endsWith("@gmail.com") || "Gmail addresses are not allowed"
-                                        // }
-                                })}
-                                aria-invalid={errors.email ? "true" : "false"}
-                                className='py-1 px-3 outline-1 outline outline-mainColor-500 rounded-md text-LightColor-400 bg-transparent placeholder:text-LightColor-700'
-                        />
+                        <span className='flex '>
+                                <input
+                                        type="email"
+                                        name="email"
+                                        id="email"
+                                        placeholder='sample@email.com'
+                                        {...register("email", {
+                                                required: "Email Address is required",
+                                                pattern: {
+                                                        value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/,
+                                                        message: "Please enter a valid email address"
+                                                },
+                                                minLength: {
+                                                        value: 7,
+                                                        message: "Email must be at least 7 characters long"
+                                                },
+                                                maxLength: {
+                                                        value: 50,
+                                                        message: "Email cannot exceed 50 characters"
+                                                },
+                                                // validate: {
+                                                //         notGmail: value =>
+                                                //                 !value.endsWith("@gmail.com") || "Gmail addresses are not allowed"
+                                                // }
+                                        })}
+                                        aria-invalid={errors.email ? "true" : "false"}
+                                        className='py-1 px-3 outline-1 outline outline-mainColor-500 rounded-md rounded-r-none text-LightColor-400 bg-transparent placeholder:text-LightColor-700'
+                                />
+                                <input
+                                        type="submit"
+                                        value="Subscribe"
+                                        className='py-2 px-6 rounded-md rounded-l-none border-solid border border-mainColor-500 outline-1 outline outline-mainColor-500 text-sm text-DarkColor-800 font-semibold bg-mainColor-500 cursor-pointer transition-all duration-300 hover:text-mainColor-400 hover:bg-DarkColor-700'
+                                />
+                        </span>
                         {errors.email && 
                                 <span 
                                         role="alert"
                                         className='text-[#c00000] text-center text-xs'
                                 >
                                         {errors.email?.message}
-                                </span>}
-                        <input
-                                type="submit"
-                                value="Subscribe"
-                                className='py-2 px-10 rounded-md text-DarkColor-800 font-semibold bg-mainColor-500 cursor-pointer transition-all duration-300 hover:text-mainColor-400 hover:bg-DarkColor-700'
-                        />
+                                </span>
+                        }
 
                         {status === 'loading' && <p className="text-mainColor-800 text-xs text-center">Loading...</p>}
                         {status === 'success' && <p className="text-[#009100] text-xs text-center">Success! Check your email to confirm subscription.</p>}
