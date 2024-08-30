71785a35a8e6739340574f536c053b2abcc7f9ed
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-08-30 21:16:46 +0100

    Create newsletterSec.jsx

---
+++ src/components/footer/newsletterSec.jsx
@@ -0,0 +1,14 @@
+'use client'
+import React from 'react'
+import Newsletter from '../forms/newsletter'
+
+export default function NewsletterSec() {
+        return (
+                <>
+                        <span className='text-center'>
+                                Stay up to date on all the Ai Trends news!
+                        </span>
+                        <Newsletter />
+                </>
+        )
+}
