d90e2a668c4d63aaaa4cc58b162e1d0d475d82e6
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-09-01 23:32:43 +0100

    Create resouces.js

---
+++ src/contentCopy/resouces.js
@@ -0,0 +1,62 @@
+export const allRes = [
+        { 
+                id: 1, 
+                category: `Templates and toolkits`,
+                title: `2024 social media daily post template`,
+                slug: {
+                        href: `/2024-social-media-daily-post-template`,
+                        title: `2024 Social Media Daily Post Template`,
+                        alt: `2024 Social Media Daily Post Template`,
+                }
+        },
+        { 
+                id: 2, 
+                category: `E-Books and guides`,
+                title: `10 top tips for creating evergreen contents`,
+                slug: {
+                        href: `/10-top-tips-for-creating-evergreen-contents`,
+                        title: `10 top tips for creating evergreen contents`,
+                        alt: `10 top tips for creating evergreen contents`,
+                }
+        },
+        { 
+                id: 3, 
+                category: `Online courses`,
+                title: `Instagram Ads 101`,
+                slug: {
+                        href: `/Instagram-ads-101`,
+                        title: `Instagram Ads 101`,
+                        alt: `Instagram Ads 101`,
+                }
+        },
+        { 
+                id: 4, 
+                category: `Templates and toolkits`,
+                title: `Templates for your professional resume`,
+                slug: {
+                        href: `/templates-for-your-professional-resume`,
+                        title: `Templates for your professional resume`,
+                        alt: `Templates for your professional resume`,
+                }
+        },
+        { 
+                id: 5, 
+                category: `E-Books and guides`,
+                title: `The act of public speaking`,
+                slug: {
+                        href: `/the-act-of-public-speaking`,
+                        title: `The act of public speaking`,
+                        alt: `The act of public speaking`,
+                }
+        },
+        { 
+                id: 6, 
+                category: `Online courses`,
+                title: `Creating your campaign videos using AI`,
+                slug: {
+                        href: `/creating-your-campaign-videos-using-AI`,
+                        title: `Creating your campaign videos using AI`,
+                        alt: `Creating your campaign videos using AI`,
+                }
+        },
+]
