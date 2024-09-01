c503ad4fae59bdee757fdf3126e62962b299f549
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-09-01 23:33:05 +0100

    Update navBar.jsx

--- src/components/headerNav/navBar.jsx
+++ src/components/headerNav/navBar.jsx
@@ -1,24 +1,30 @@
 'use client'
 import React, { useState } from 'react'
 import Logo from './logo'
-import Link from 'next/link'
-import PriBtn from '../button/priBtn'
 import MobileNav from './mobileNav'
-import { menuLinks } from '../../contentCopy/headerNavCopy'
+import ResourcesPopUp from '../popUps/resourcesPopUp'
+import DesktopNav from './desktopNav'
 
 export default function NavBar() {
 
         const [openNav, setOpenNav] = useState(false);
+        const [openRes, setOpenRes] = useState(false);
 
         const handleMobileMenu = () => {
                 setOpenNav(!openNav);
+        }
+
+        const handleOpenRes = () => {
+                setOpenRes(!openRes);
         }
 
         return (
                 <>
                         <header className='fixed top-5 left-2/4 translate-x-[-50%] flex justify-between items-center gap-5 p-5 max-w-[1440px] w-[96%] mx-auto bg-LightColor-100 rounded-lg border border-solid border-LightColor-300 z-10'>
                                 <Logo />
-                                <DesktopNav />
+                                <DesktopNav 
+                                        handleOpenRes={handleOpenRes}
+                                />
                                 <span
                                         className='min-[901px]:hidden text-xs font-semibold uppercase cursor-pointer'
                                         onClick={handleMobileMenu}
@@ -29,38 +35,9 @@ export default function NavBar() {
                         <MobileNav
                                 onClick={handleMobileMenu}
                                 openNav={openNav}
+                                handleOpenRes={handleOpenRes}
                         />
+                        {openRes ? (<ResourcesPopUp />) : ''}                        
                 </>
-        )
-}
-
-const DesktopNav = () => {
-        return (
-                <div className='flex items-center gap-5 text-base max-[900px]:hidden'>
-                        <Nav />
-                        <PriBtn
-                                label={'Register Now'}
-                                href={'/'}
-                        />
-                </div>
-        )
-}
-
-const Nav = () => {
-        return (
-                <nav>
-                        <ul className='flex gap-5'>
-                                {menuLinks.map((items) => (
-                                        <li key={items.id}>
-                                                <Link
-                                                        href={items.url}
-                                                        title={items.name}
-                                                >
-                                                        {items.name}
-                                                </Link>
-                                        </li>
-                                ))}
-                        </ul>
-                </nav>
         )
 }
