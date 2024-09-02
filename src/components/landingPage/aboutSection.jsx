930fdc6f2c9d6df54a06a3d5cac8ed48ceef7e38
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-09-02 01:02:47 +0100

    Update aboutSection.jsx

--- src/components/landingPage/aboutSection.jsx
+++ src/components/landingPage/aboutSection.jsx
@@ -7,6 +7,7 @@ export default function AboutSection() {
                 <section
                         className="grid place-content-center py-20 sm:py-40 max-w-screen bg-mainColor-100 overflow-x-hidden bg-no-repeat bg-center bg-cover bg-gradient-to-r from-mainColor-100 via-transparent to-mainColor-100"
                         style={{ backgroundImage: 'url(/images/bg-grid-lines-plain.svg)' }}
+                        id={`about`}
                 >
                         <div className="grid grid-cols-1 sm:grid-cols-2 content-between items-center gap-12 sm:gap-10 px-5 max-w-[1440px] w-[95%] mx-auto text-center">
                                 <div>
