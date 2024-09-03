62818abbe06ea41a27b6d160d03bd3692d88ae93
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-09-03 23:46:13 +0100

    Update popUpBg.jsx

--- src/components/popUps/popUpBg.jsx
+++ src/components/popUps/popUpBg.jsx
@@ -1,9 +1,16 @@
+import { CloseIcon } from '@/icons'
 import React from 'react'
 
-export default function PopUpBg({ children }) {
+export default function PopUpBg({ children, clickClose }) {
         return (
-                <div className='fixed inset-0 size-full grid place-content-center bg-DarkColor-900/85 backdrop-blur p-10 z-10'>
-                        <div className='max-w-4xl bg-mainColor-100 rounded-xl'>
+                <div className='fixed inset-0 size-full bg-DarkColor-900/85 backdrop-blur p-10 z-10'>
+                        <div className='relative max-w-4xl size-full py-10 px-5 m-auto bg-mainColor-100 rounded-xl'>
+                                <div 
+                                        onClick={clickClose}
+                                        className='absolute top-5 right-5 p-2 bg-mainColor-700 text-DarkColor-500 border-solid border border-DarkColor-500 rounded-full cursor-pointer'
+                                >
+                                        <CloseIcon/>
+                                </div>
                                 {children}
                         </div>
                 </div>
