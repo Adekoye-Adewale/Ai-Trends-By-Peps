9d187ba815069a9c9b7bb5fc91a61b8b36f790ae
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-09-04 22:25:29 +0100

    Create contactPopUp.jsx

---
+++ src/components/popUps/contactPopUp.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PopUpBg from './popUpBg'
+import ContactForm from '../forms/contactForm'
+
+export default function ContactPopUp({ setOpenPop, openPop}) {
+
+        const handleClickClose = () => {
+                setOpenPop(!openPop);
+        }
+
+        return (
+                <PopUpBg clickClose={handleClickClose}>
+                        <ContactForm/>
+                </PopUpBg>
+        )
+}
