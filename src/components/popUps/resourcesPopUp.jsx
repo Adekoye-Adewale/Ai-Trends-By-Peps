f793b23d19886de64014cdf20489b609a2bc3ca0
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-11-18 22:18:57 +0100

    Update resourcesPopUp.jsx

--- src/components/popUps/resourcesPopUp.jsx
+++ src/components/popUps/resourcesPopUp.jsx
@@ -8,11 +8,11 @@ export default function ResourcesPopUp({ closeResPopUp }) {
                 <div className='fixed inset-0 bg-DarkColor-900/85 backdrop-blur z-[2] animate-fade-in-250'>
                         <div 
                                 onClick={closeResPopUp}
-                                className='block md:hidden text-mainColor-300 bg-DarkColor-700 absolute top-[115px] right-[10px] p-1 rounded-full'
+                                className='block  text-mainColor-100 bg-[#cc0000] absolute top-[115px] right-[10px] p-1 rounded-full cursor-pointer'
                         >
                                 <CloseIcon/>
                         </div>
-                        <div className='max-w-[1440px] w-[96%] mx-auto mt-32'>
+                        <div className='max-w-[1440px] w-[96%] mx-auto mt-32 overflow-y-auto'>
                                 <Cards/>
                         </div>
                 </div>
