d6ce0262819a9ae845886960e664f173973eddd8
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-11-18 22:18:50 +0100

    Update popUpBg.jsx

--- src/components/popUps/popUpBg.jsx
+++ src/components/popUps/popUpBg.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 export default function PopUpBg({ children, clickClose }) {
         return (
-                <div className='fixed inset-0 size-full bg-DarkColor-900/85 backdrop-blur p-10 z-10'>
-                        <div className='relative max-w-4xl size-full py-10 px-5 m-auto bg-mainColor-100 rounded-xl'>
+                <div className='fixed inset-0 size-full bg-DarkColor-900/85 backdrop-blur p-10 z-10 overflow-hidden'>
+                        <div className='relative max-w-4xl size-full py-10 px-5 m-auto bg-mainColor-100 rounded-xl overflow-y-auto'>
                                 <div 
                                         onClick={clickClose}
                                         className='absolute top-5 right-5 p-2 bg-mainColor-700 text-DarkColor-500 border-solid border border-DarkColor-500 rounded-full cursor-pointer'
