dd6dba4403752cb71c015769ec1f0dee0693cff5
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-11-18 22:55:54 +0100

    Update testimonials.js

--- src/contentCopy/testimonials.js
+++ src/contentCopy/testimonials.js
@@ -16,7 +16,7 @@ export const TestimonialCopy = [
                 id: 2,
                 name: "Greer Yeaton",
                 role: `Human Resource Manager`,
-                testimony: `I learnt how to use AI to my benefit, writing productive and optimized code, also building a better profile on LinkedIn, and it gave me the confidence to write content.`,
+                testimony: `I learnt a way to use AI to my benefit, writing effective and optimized code, additionally constructing a higher profile on LinkedIn, and it gave me the self assurance to put in writing content.`,
                 img: {
                         src: `/images/female-john-doe.webp`,
                         alt: `Greer Yeaton`,
