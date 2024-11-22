005d452f44c9cb0e2e38f548c0229c83558584f4
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-11-22 10:44:15 +0100

    Create listSection.jsx

---
+++ src/components/resources/listSection.jsx
@@ -0,0 +1,90 @@
+import Image from 'next/image'
+import React, { Fragment } from 'react'
+import { SubTitle, Title } from '../text'
+import Link from 'next/link'
+
+export default function ListSection({ content, secTitle, link }) {
+        return (
+                <section className='grid gap-5 py-20 overflow-clip'>
+                        <div className='max-w-[1440px] w-[95%] mx-auto grid gap-5'>
+                                <div className='flex justify-between items-center'>
+                                        <div>
+                                                <Title label={secTitle}/>
+                                        </div>
+                                        <div>
+                                                <Link 
+                                                        href={link} 
+                                                        title='open all'
+                                                        target='__blank'
+                                                >
+                                                        Load more
+                                                </Link>
+                                        </div>
+                                </div>
+                                <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4'>
+                                        {content.slice(0, 4).map(( item ) => (
+                                                <Fragment key={item.id}>
+                                                        <Card 
+                                                                img={item.img} 
+                                                                label={item.title} 
+                                                                slug={item.slug}
+                                                        />
+                                                </Fragment>
+                                        ))}
+                                </div>
+                        </div>
+                </section>
+        )
+}
+
+export function CategoryListSection({ content, secTitle, link }) {
+        return (
+                <section className='grid gap-5 py-20 overflow-clip'>
+                        <div className='max-w-[1440px] w-[95%] mx-auto grid gap-5'>
+                                <div className='flex justify-between items-center'>
+                                        <div>
+                                                <Title label={secTitle} />
+                                        </div>
+                                        <div>
+                                                <Link
+                                                        href={link}
+                                                        title='open all'
+                                                        target='__blank'
+                                                >
+                                                        Load more
+                                                </Link>
+                                        </div>
+                                </div>
+                                <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4'>
+                                        {content.map((item) => (
+                                                <Fragment key={item.id}>
+                                                        <Card
+                                                                img={item.img}
+                                                                label={item.title}
+                                                                slug={item.slug}
+                                                        />
+                                                </Fragment>
+                                        ))}
+                                </div>
+                        </div>
+                </section>
+        )
+}
+
+const Card =({ img, label, slug })=> {
+        return(
+                <div className='rounded-xl border-2 border-solid border-DarkColor-300 bg-mainColor-100 shadow-[6px_6px_0px_0px_#000204] transition-all duration-300 hover:shadow-[-6px_-6px_0px_0px_#000204] cursor-default overflow-clip hover:bg-mainColor-200'>
+                        <Image 
+                                {...img}
+                        />
+                        <div className='p-3 [&>h3]:mb-2 [&>h3]:text-lg sm:[&>h3]:text-xl [&>h3]:font-medium [&>h3]:text-DarkColor-700 [&>a]:text-sm '>
+                                <SubTitle 
+                                        label={label}
+                                />
+                                <Link {...slug}>
+                                        Read more
+                                </Link>
+                        </div>
+                </div>
+        )
+}
