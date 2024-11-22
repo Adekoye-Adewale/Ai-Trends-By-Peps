20ed62bbb966e3ed5c0c38f5b058a55314f4798e
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-11-22 10:44:20 +0100

    Create index.js

---
+++ src/components/resources/index.js
@@ -0,0 +1,5 @@
+import { TopBanner, Banner } from "./banner"
+import CategoryPageComponent from "./category"
+import ListSection, {CategoryListSection} from "./listSection"
+
+export { TopBanner, Banner, ListSection, CategoryPageComponent, CategoryListSection }
