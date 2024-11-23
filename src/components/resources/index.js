31c3217fee1f425016129f06164f778194620682
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-11-23 11:11:46 +0100

    Update index.js

--- src/components/resources/index.js
+++ src/components/resources/index.js
@@ -1,5 +1,15 @@
 import { TopBanner, Banner } from "./banner"
 import CategoryPageComponent from "./category"
 import ListSection, {CategoryListSection} from "./listSection"
+import ResPage from "./resourcePg"
+import ResourceBody from "./resourceBody"
 
-export { TopBanner, Banner, ListSection, CategoryPageComponent, CategoryListSection }
+export { 
+        TopBanner, 
+        Banner, 
+        ListSection, 
+        CategoryPageComponent, 
+        CategoryListSection, 
+        ResPage, 
+        ResourceBody 
+};
