d2d5a98325f8f542189a2966614bc735f08699a1
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-11-23 13:51:58 +0100

    Update logo.jsx

--- src/components/headerNav/logo.jsx
+++ src/components/headerNav/logo.jsx
@@ -1,17 +1,23 @@
 import Image from 'next/image'
 import React from 'react'
 import AiTrendsLogo from '../../../public/AiTrendsLogo.svg'
+import Link from 'next/link'
 
 
 export default function Logo() {
         return (
-                <Image
-                        src={AiTrendsLogo}
-                        alt={'Ai Trends Logo'}
-                        title={'Ai Trends Logo'}
-                        width={200}
-                        height={200}
-                        className='w-12 h-12 rounded-md'
-                />
+                <Link 
+                        href={`/`}
+                        title='AI Trends Logo'
+                >
+                        <Image
+                                src={AiTrendsLogo}
+                                alt={'Ai Trends Logo'}
+                                title={'Ai Trends Logo'}
+                                width={200}
+                                height={200}
+                                className='w-12 h-12 rounded-md'
+                        />
+                </Link>
         )
 }
