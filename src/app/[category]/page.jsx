c6054486e2a7657298ff8a48804f7b059e6f30f2
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-11-23 11:11:54 +0100

    Create page.jsx

---
+++ src/app/[category]/page.jsx
@@ -0,0 +1,34 @@
+import { CategoryPageComponent } from "@/components/resources"
+import { allRes } from "@/contentCopy/resouces";
+
+const resImg = {
+        src: `/images/Sunny.png`,
+        alt: `AI trend resource page image`,
+        title: `AI trend resource page image`,
+        width: 1400,
+        height: 800
+}
+
+export default async function CategoryPage({ params }) {
+        const categorySlug = (await params).category
+
+        const filteredContent = allRes.filter(
+                item => item.category.slug === `/${categorySlug}`
+        );
+
+        const categoryName = filteredContent.length > 0
+                ? filteredContent[0].category.name
+                : "Category Not Found";
+
+
+        return (
+                <div>
+                        <CategoryPageComponent
+                                bgImg={resImg}
+                                pageTitle={categoryName}
+                                desc={`Explore resources in the ${categoryName} category.`}
+                                content={filteredContent}
+                        />
+                </div>
+        )
+}
