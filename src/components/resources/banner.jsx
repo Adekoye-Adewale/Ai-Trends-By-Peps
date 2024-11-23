2d41b25182f970690e1c8252b4994e143bef88ea
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-11-23 11:11:01 +0100

    Update banner.jsx

--- src/components/resources/banner.jsx
+++ src/components/resources/banner.jsx
@@ -4,14 +4,14 @@ import { Headline, Paragraph, Title } from '../text'
 
 export function TopBanner({ img, title, label }) {
         return (
-                <div className='relative pt-40 pb-20 w-screen min-h-[500px] bg-DarkColor-700'>
+                <div className='relative pt-40 pb-20 min-h-[500px] bg-DarkColor-700'>
                         <Image 
                                 {...img}
                                 className='absolute top-0 left-0 size-full object-cover z-[0] brightness-[.25]'
                         />
                         <div className='relative max-w-[1440px] w-[95%] mx-auto grid gap-2 z-[1] [&>h1]:text-LightColor-200 [&>p]:text-DarkColor-200'>
-                                <Headline label={title}/>
-                                <Paragraph label={label}/>
+                                <Headline label={title}/>                                
+                                {title !== 'Category Not Found' && (<Paragraph label={label} />)}
                         </div>
                 </div>
         )
@@ -19,7 +19,7 @@ export function TopBanner({ img, title, label }) {
 
 export function Banner({ img, title, label }) {
         return (
-                <div className='relative pt-20 pb-20 w-screen min-h-[500px] bg-DarkColor-700'>
+                <div className='relative pt-20 pb-20 min-h-[500px] bg-DarkColor-700'>
                         <Image
                                 {...img}
                                 className='absolute top-0 left-0 size-full object-cover z-[1] brightness-[.25]'
