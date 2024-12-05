79b1531e62c11be51cde980b7c3d83f656968882
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-12-05 02:44:10 +0100

    Update page.jsx

--- src/app/resources/[resource]/page.jsx
+++ src/app/resources/[resource]/page.jsx
@@ -1,15 +1,51 @@
 import React from 'react'
+import { client } from "@/sanity/client";
+import { PortableText } from "next-sanity";
+import imageUrlBuilder from "@sanity/image-url";
 import { ResPage } from '@/components/resources'
-import { allRes } from '@/contentCopy/resouces';
+import { ArticleNotFound } from '@/components/errorPage';
+
+const POST_QUERY = `*[_type == "post" && slug.current == $resource][0]`;
+
+const { projectId, dataset } = client.config();
+const urlFor = (source) =>
+        projectId && dataset
+                ? imageUrlBuilder({ projectId, dataset }).image(source)
+                : null;
+
+const options = { next: { revalidate: 30 } };
 
 export default async function ResourcePage({ params }) {
-        const articleSlug = (await params).resource
+        const res = await client.fetch(POST_QUERY, params, options);
 
-        const filteredContent = allRes.filter(
-                item => item.slug.href === `/${articleSlug}`
-        );
+        if (!res) {
+                return (
+                        <ArticleNotFound/>
+                );
+        }
 
-        const article = filteredContent[0];
+        const postImageUrl = res.image
+                ? urlFor(res.image)?.width(550).height(310).url()
+                : null;
+
+        const article = {
+                title: res?.title,
+                publishedAt: res?.publishedAt,
+                featured: res?.featured,
+                category: res?.category,
+                img: {
+                        src: postImageUrl,
+                        alt: res?.title,
+                        title: res?.title,
+                        width: 1000,
+                        height: 400,
+                },
+                body: res?.body,
+                commentsEnable: res?.commentsEnable,
+                relatedPosts: res?.relatedPosts,
+        }
+
+        console.log('===================================', article)
 
         return (
                 <main>
