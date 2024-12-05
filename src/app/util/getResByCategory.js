a2539a1e69dc7778abb8b0a7acb33d854a9542fd
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-12-05 02:44:17 +0100

    Update getResByCategory.js

--- src/app/util/getResByCategory.js
+++ src/app/util/getResByCategory.js
@@ -1,5 +1,17 @@
 import { allRes } from "@/contentCopy/resouces";
+import { client } from "@/sanity/client";
 
+const POSTS_QUERY = `*[
+  _type == "post"
+  && defined(slug.current)
+]|order(publishedAt desc)[0...12]{_id, title, slug, category, publishedAt, image, body, featured, category, commentsEnable, relatedPosts}`;
+
+const options = { next: { revalidate: 30 } };
+
+export default async function getAllPosts() {
+        const posts = await client.fetch(POSTS_QUERY, {}, options);
+        return posts
+}
 
 export const filterByCategory = (category) => {
         return allRes.filter(item => item.category?.name === category);
