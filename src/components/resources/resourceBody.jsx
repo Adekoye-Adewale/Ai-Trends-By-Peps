10619784c597d2025bc9bc901ff903f97eb96571
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-12-05 02:44:27 +0100

    Update resourceBody.jsx

--- src/components/resources/resourceBody.jsx
+++ src/components/resources/resourceBody.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import DOMPurify from "dompurify";
+import { PortableText } from "next-sanity";
 
 export default function ResourceBody({ content }) {
 
@@ -9,9 +9,10 @@ export default function ResourceBody({ content }) {
                 <div className='grid gap-5 py-20 overflow-clip'>
                         <div className='max-w-[1440px] w-[95%] mx-auto'>
                                 <article 
-                                        dangerouslySetInnerHTML={{ __html: content }}
-                                        className='[&>h1]:text-3xl [&>h1]:font-extrabold sm:[&>h1]:text-5xl [&>h1]:my-3 [&>h2]:text-3xl sm:[&>h2]:text-5xl [&>h2]:my-3 [&>h3]:text-2xl sm:[&>h3]:text-4xl [&>h3]:my-2 [&>h4]:text-2xl sm:[&>h4]:text-3xl [&>h4]:my-2 [&>h5]:text-xl sm:[&>h5]:text-2xl [&>h5]:my-2 [&>ol]:my-2 [&>ol]:ml-2 [&>ul]:my-2 [&>ul]:ml-2 [&>p]:my-2 [&>img]:size-full [&>a]:text-mainColor-800 [&>*>a]:text-mainColor-800'
+                                        // dangerouslySetInnerHTML={{ __html: content }}
+                                        className='[&>h1]:text-3xl [&>h1]:font-extrabold sm:[&>h1]:text-5xl [&>h1]:my-3 [&>h2]:text-3xl sm:[&>h2]:text-5xl [&>h2]:my-3 [&>h3]:text-2xl sm:[&>h3]:text-4xl [&>h3]:my-2 [&>h4]:text-2xl sm:[&>h4]:text-3xl [&>h4]:my-2 [&>h5]:text-xl sm:[&>h5]:text-2xl [&>h5]:my-2 [&>ol]:my-2 [&>ol]:ml-2 [&>ul]:my-2 [&>ul]:ml-2 [&>p]:my-2 [&>img]:size-full [&>a]:text-red-800 [&>*>a]:text-red-800'
                                 >
+                                        {Array.isArray(content) && <PortableText value={content} />}
                                 </article>
                         </div>
                 </div>
