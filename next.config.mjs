438a87d1f89fff28af31acffd7b0ab24487986eb
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-12-05 02:43:46 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,16 @@
 import { withNextVideo } from "next-video/process";
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+        images: {
+                remotePatterns: [
+                        {
+                                protocol: 'https',
+                                hostname: 'cdn.sanity.io',
+                                port: '', 
+                                pathname: '/images/**',
+                        },
+                ],
+        },
+};
 
 export default withNextVideo(nextConfig, { folder: 'videos' });
