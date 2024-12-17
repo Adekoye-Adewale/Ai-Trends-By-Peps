143a59cf4b238fc5c7959f1d1fd0c9721c0878ab
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-12-17 20:19:20 +0100

    Create route.js

---
+++ src/app/api/email/route.js
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server"
+import { ConnectDB } from "../../../../lib/config/db"
+import EmailModel from "../../../../lib/models/contactFormModel"
+
+const LoadDB = async () => {
+        await ConnectDB()
+}
+LoadDB()
+
+export async function POST(request) {
+        const formData = await request.formData()
+        const contactFormData = {
+                fullname: `${formData.get('fullname')}`,
+                email: `${formData.get('email')}`,
+                phoneNumber: `${formData.get('phoneNumber')}`,
+                resourceOfInterest: `${formData.get('resourceOfInterest')}`,
+                state: `${formData.get('state')}`,
+                country: `${formData.get('country')}`,
+                fieldOfWork: `${formData.get('fieldOfWork')}`,
+        }
+        await EmailModel.create(contactFormData)
+        return NextResponse.json({
+                success: true,
+                msg: 'Form submited successfully'
+        })
+}
