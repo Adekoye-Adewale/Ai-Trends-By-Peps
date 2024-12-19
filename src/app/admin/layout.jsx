de1150384d65cd9bb52f0b5c288a63de616ff71a
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-12-19 20:51:56 +0100

    Create layout.jsx

---
+++ src/app/admin/layout.jsx
@@ -0,0 +1,16 @@
+import PageContent from "@/components/admin/pageContent";
+import Sidebar from "@/components/admin/sidebar";
+
+export default function AdminLayout({ children }) {
+        return (
+                <main className='flex admin'>
+                        <Sidebar/>
+                        <div className="w-full">
+                                <PageContent/>
+                                <div className="py-10 px-5 overflow-x-hidden overflow-y-auto">
+                                        {children}
+                                </div>
+                        </div>
+                </main>
+        )
+}
