9ff2e69d8c5717e82a266f8f2c8902efbb30ac77
Adekoye Adewale <adekoyeadewale@gmail.com>
2025-01-12 20:31:25 +0100

    Create page.jsx

---
+++ src/app/admin/newsletter/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NewsletterPage() {
+        return (
+                <div>
+                        Newsletter
+                </div>
+        )
+}
