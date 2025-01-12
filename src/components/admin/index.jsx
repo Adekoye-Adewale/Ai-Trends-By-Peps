a70dd106be9473d422e87dfe743ded7da845bf6c
Adekoye Adewale <adekoyeadewale@gmail.com>
2025-01-12 20:31:20 +0100

    Create index.jsx

---
+++ src/components/admin/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AdminDashboard() {
+        return (
+                <div className='flex'>
+                        Main Page
+                </div>
+        )
+}
