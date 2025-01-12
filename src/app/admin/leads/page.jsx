73eb8406ad4fd23f4fa2021af700ab1c66aa9c13
Adekoye Adewale <adekoyeadewale@gmail.com>
2025-01-12 20:31:28 +0100

    Create page.jsx

---
+++ src/app/admin/leads/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import TableComponent from '@/components/admin/table'
+import axios from 'axios';
+import { processLeadsData } from '@/app/util/getLeads';
+
+export default async function LeadsPage() {
+        const { data } = await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/api/email`);
+        const leadsData = processLeadsData(data.contactFormData)
+        return (
+                <div>
+                        <TableComponent
+                                leadsData={leadsData}
+                        />
+                </div>
+        )
+}
