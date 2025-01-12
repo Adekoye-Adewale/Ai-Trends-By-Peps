031fb735385f2c9454f2155f08e04c10ad098c82
Adekoye Adewale <adekoyeadewale@gmail.com>
2025-01-12 20:31:22 +0100

    Create page.jsx

---
+++ src/app/admin/page.jsx
@@ -0,0 +1,8 @@
+import AdminDashboard from '@/components/admin'
+import React from 'react'
+
+export default async function page() {
+        return (
+                <AdminDashboard/>
+        )
+}
