5e795282c6fde60883eaae1e89b11f56e0c1773f
Adekoye Adewale <adekoyeadewale@gmail.com>
2025-01-29 22:39:44 +0100

    Update page.jsx

--- src/app/admin/leads/page.jsx
+++ src/app/admin/leads/page.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
-import TableComponent from '@/components/admin/table'
-import axios from 'axios';
-import { processLeadsData } from '@/app/util/getLeads';
+// import React from 'react'
+// import TableComponent from '@/components/admin/table'
+// import axios from 'axios';
+// import { processLeadsData } from '@/app/util/getLeads';
 
-export default async function LeadsPage() {
-        const { data } = await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/api/email`);
-        const leadsData = processLeadsData(data.contactFormData)
+// export default async function LeadsPage() {
+//         const { data } = await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/api/email`);
+//         const leadsData = processLeadsData(data.contactFormData)
+//         return (
+//                 <div>
+//                         <TableComponent
+//                                 leadsData={leadsData}
+//                         />
+//                 </div>
+//         )
+// }
+
+
+import React from 'react'
+
+export default function LeadsPage() {
         return (
-                <div>
-                        {/* <TableComponent
-                                leadsData={leadsData}
-                        /> */}
-                </div>
+                <div>Lead page</div>
         )
 }
