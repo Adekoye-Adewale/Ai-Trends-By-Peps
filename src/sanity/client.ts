e0ba9d2eeef8e9eda21410024d2f09f67e875042
Adekoye Adewale <adekoyeadewale@gmail.com>
2025-02-08 14:53:54 +0100

    Update client.ts

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from "next-sanity";
 
 export const client = createClient({
-        projectId: "6g8m95hv",
+        projectId: `${process.env.SANITY_ID}`,
         dataset: "production",
         apiVersion: "2024-01-01",
         useCdn: false,
