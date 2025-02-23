d3e91a6c666259cfd2006e436837b677e1e9ed15
Adekoye Adewale <adekoyeadewale@gmail.com>
2025-02-23 13:39:54 +0100

    Create sampleData.js

---
+++ src/contentCopy/sampleData.js
@@ -0,0 +1,24 @@
+export const leadData = [
+        { 
+                _id: "6760a5fa120ccd6f07b08d7d",
+                fullname: "Tek Han",
+                email: "han@manutd.com", 
+                phoneNumber: "+2347012121212", 
+                resourceOfInterest: "templates", 
+                state: "Jos", 
+                country: "Nigeria", 
+                fieldOfWork: "Technology", 
+                date: "2024-12-16T22:13:14.243+00:00", 
+        },
+        {
+                _id: "6760afeb120ccd6f07b08d7f",
+                fullname: "Temi Fam",
+                email: "tem@famil.lili",
+                phoneNumber: "+2347098986756",
+                resourceOfInterest: "courses",
+                state: "Ogun",
+                country: "Nigeria",
+                fieldOfWork: "Technology",
+                date: "2024-12-16T22:13:14.243+00:00",
+        }
+]
