da1ba341228870e80da71460fa6d0f7de9903680
Adekoye Adewale <adekoyeadewale@gmail.com>
2025-02-24 11:49:07 +0100

    Update index.jsx

--- src/components/admin/index.jsx
+++ src/components/admin/index.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import MainPageCards, { TitleCard } from './mainPageCards'
+import { leadsDataLength } from '@/app/util/fetchLeadsData'
 
 export default function AdminDashboard() {
         return (
                 <div className='flex'>
-                        Main Page
+                        <div>
+                                <LeadSec/>
+                        </div>
+                </div>
+        )
+}
+
+async function LeadSec ()  {
+        const dataLength = await leadsDataLength()
+        
+        return (
+                <div className='flex border-solid border-2 border-mainColor-900 divide-x-2 divide-solid'>
+                        <TitleCard 
+                                title={"User Data"} 
+                                desc={"Showing summary of total user data"}
+                        />
+                        <MainPageCards 
+                                title={"Total Leads"} 
+                                number={dataLength}
+                        />
+                        <MainPageCards 
+                                title={"Total Newsletter"} 
+                                number={"3568"}
+                        />
                 </div>
         )
 }
