072fae1f8d04013d5c0a2b34ecc5bd8b0bd792b3
Adekoye Adewale <adekoyeadewale@gmail.com>
2025-02-25 21:49:45 +0100

    Update paragraph.jsx

--- src/components/text/paragraph.jsx
+++ src/components/text/paragraph.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-export default function Paragraph({ label }) {
+export default function Paragraph({ label, className }) {
         return (
-                <p className='text-base sm:text-lg font-normal text-pretty'>
+                <p className={`text-base sm:text-lg font-normal text-pretty ${className}`}>
                         {label}
                 </p>
         )
