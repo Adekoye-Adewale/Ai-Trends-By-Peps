6a1ac82ced8a7f1186fd0a7b6cb953502fa1c22f
Adekoye Adewale <adekoyeadewale@gmail.com>
2025-02-25 20:34:05 +0100

    handle errors properly

--- src/app/admin/leads/page.jsx
+++ src/app/admin/leads/page.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+'use client'
+import React, { useState, useEffect } from 'react';
 import TableComponent from '@/components/admin/table'
 import { fetchLeadsData } from '@/app/util/fetchLeadsData'
 import { processLeadsData } from '@/app/util/getLeads'
 
-export default async function LeadsPage() {
-        const data = await fetchLeadsData();
-        const leadsData = processLeadsData(data.contactFormData)
+export default function LeadsPage() {
+        const [leadsData, setLeadsData] = useState(null);
+        const [loading, setLoading] = useState(true);
+        const [error, setError] = useState(null);
+
+        useEffect(() => {
+                const fetchData = async () => {
+                        try {
+                                const data = await fetchLeadsData();
+                                console.log('=====', data)
+
+                                if (!data || !data.contactFormData || data.contactFormData.length === 0) {
+                                        setError('No leads data available.');
+                                        setLeadsData([]);
+                                } else {
+                                        const processedData = processLeadsData(data.contactFormData);
+                                        setLeadsData(processedData);
+                                        setError(null);
+                                }
+                        } catch (err) {
+                                setError('Failed to load data. Please try again later.');
+                                setLeadsData([]);
+                        } finally {
+                                setLoading(false);
+                        }
+                };
+
+                fetchData();
+        }, []);
+
+        if (loading) {
+                return <div>Loading...</div>; 
+        }
+
+        if (error) {
+                return <div>{error}</div>; 
+        }
 
         return (
                 <div>
