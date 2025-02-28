28687f2d69c13b4563e6efc9a83f4ee88849f254
Adekoye Adewale <adekoyeadewale@gmail.com>
2025-02-28 16:33:29 +0100

    Update fetchLeadsData.js

--- src/app/util/fetchLeadsData.js
+++ src/app/util/fetchLeadsData.js
@@ -1,7 +1,16 @@
 'use server'
+import { revalidatePath } from 'next/cache';
+
 export async function fetchLeadsData() {
+        const url = `${process.env.NEXT_PUBLIC_BASE_URL}/api/email`;
         try {
-                const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/email`);
+                const response = await fetch(url, {
+                        method: "GET",
+                        headers: {
+                                "Content-Type": "application/json",
+                        },
+                        next: { revalidate: 60 }
+                });
                 if (!response.ok) {
                         if (response.status === 403) {
                                 throw new Error('Invalid API key or access forbidden.');
@@ -14,6 +23,10 @@ export async function fetchLeadsData() {
                 console.error(error);
                 return [];
         }
+}
+
+export async function revalidateLeads() {
+        revalidatePath('/admin/leads'); 
 }
 
 export async function leadsDataLength() {
