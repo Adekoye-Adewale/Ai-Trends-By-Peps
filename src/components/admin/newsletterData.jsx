b7b3e248fe34c771e9e39d82b281ea81c84bd5ad
Adekoye Adewale <adekoyeadewale@gmail.com>
2025-03-02 20:26:56 +0100

    Update newsletterData.jsx

--- src/components/admin/newsletterData.jsx
+++ src/components/admin/newsletterData.jsx
@@ -3,7 +3,7 @@ import NewsletterTable from './newsletterTable'
 
 export default function NewsletterData({ subscribers }) {
         return (
-                <div className="max-w-3xl mx-auto p-4">
+                <div className="max-w-3xl p-4">
                         <h1 className="text-2xl font-bold">
                                 Newsletter Subscribers
                         </h1>
